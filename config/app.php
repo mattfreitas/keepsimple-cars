73395978fcf5316bc369ab89b2a665451f144f29
Matheus Freitas <msfbr.00@gmail.com>
2022-02-21 17:00:39 -0300

    WYP: Database starting point, seeders and factories

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Providers\FakerServiceProvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -163,6 +164,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        FakerServiceProvider::class,
 
         /*
          * Package Service Providers...
